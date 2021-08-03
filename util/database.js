eb9e44b4c3122605d6278aa5f4e246eaa9738e15
Dhimas Prajaya <dhimasprajaya@gmail.com>
2021-08-03 17:48:11 +0700

    add delete and edit function

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node_first_app', 'root', 'katakunci', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node_first_app',
-  password: 'katakunci',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
