fc73da76da2a35baffb183656739af3c828b9ac7
Dhimas Prajaya <dhimasprajaya@gmail.com>
2021-08-05 10:07:00 +0700

    add fetch single

--- controllers/admin.js
+++ controllers/admin.js
@@ -34,7 +34,7 @@ exports.postAddProduct = (req, res, next) => {
 //   const prodId = req.params.productId;
 //   req.user
 //     .getProducts({ where: { id: prodId } })
-//     // Product.findByPk(prodId)
+//     // Product.findById(prodId)
 //     .then(products => {
 //       const product = products[0];
 //       if (!product) {
@@ -56,7 +56,7 @@ exports.postAddProduct = (req, res, next) => {
 //   const updatedPrice = req.body.price;
 //   const updatedImageUrl = req.body.imageUrl;
 //   const updatedDesc = req.body.description;
-//   Product.findByPk(prodId)
+//   Product.findById(prodId)
 //     .then(product => {
 //       product.title = updatedTitle;
 //       product.price = updatedPrice;
@@ -71,21 +71,22 @@ exports.postAddProduct = (req, res, next) => {
 //     .catch(err => console.log(err));
 // };
 
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then((products) => {
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products',
-      });
-    })
-    .catch((err) => console.log(err));
-};
+// exports.getProducts = (req, res, next) => {
+//   req.user
+//     .getProducts()
+//     .then(products => {
+//       res.render('admin/products', {
+//         prods: products,
+//         pageTitle: 'Admin Products',
+//         path: '/admin/products'
+//       });
+//     })
+//     .catch(err => console.log(err));
+// };
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   const prodId = req.body.productId;
-//   Product.findByPk(prodId)
+//   Product.findById(prodId)
 //     .then(product => {
 //       return product.destroy();
 //     })
--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
+
 class Product {
   constructor(title, price, description, imageUrl) {
     this.title = title;
@@ -26,7 +28,25 @@ class Product {
       .collection('products')
       .find()
       .toArray()
-      .then()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
       .catch((err) => {
         console.log(err);
       });
