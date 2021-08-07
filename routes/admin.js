e61c0bd6a74464903a7b4644762687e7c3efd65f
Dhimas Prajaya <dhimasprajaya@gmail.com>
2021-08-07 22:29:06 +0700

    pagination

--- routes/admin.js
+++ routes/admin.js
@@ -18,9 +18,14 @@ router.get('/products', isAuth, adminController.getProducts);
 router.post(
   '/add-product',
   [
-    body('title').isString().isLength({ min: 3 }).trim(),
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
     body('price').isFloat(),
-    body('description').isLength({ min: 5, max: 400 }).trim(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
   ],
   isAuth,
   adminController.postAddProduct
@@ -31,9 +36,14 @@ router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 router.post(
   '/edit-product',
   [
-    body('title').isString().isLength({ min: 3 }).trim(),
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
     body('price').isFloat(),
-    body('description').isLength({ min: 5, max: 400 }).trim(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
   ],
   isAuth,
   adminController.postEditProduct
