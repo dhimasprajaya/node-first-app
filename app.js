15958fe4d1051cd9bae285206b60c1cbe9c47b17
Dhimas Prajaya <dhimasprajaya@gmail.com>
2021-08-10 23:23:27 +0700

    prepared for deployment

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const fs = require('fs');
+// const https = require('https');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,12 +10,16 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI =
-  'mongodb+srv://dhimas:katakunci@cluster0.jo6yi.mongodb.net/shop?retryWrites=true&w=majority';
+console.log(process.env.NODE_ENV);
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.jo6yi.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
 
 const app = express();
 const store = new MongoDBStore({
@@ -21,6 +27,9 @@ const store = new MongoDBStore({
   collection: 'sessions',
 });
 const csrfProtection = csrf();
+
+// const privateKey = fs.readFileSync('-server.key');
+// const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -49,6 +58,17 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  {
+    flags: 'a',
+  }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
@@ -113,7 +133,10 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-    app.listen(3000);
+      // https
+      // .createServer({ key: privateKey, cert: certificate }, app)
+      // .listen(process.env.PORT || 3000);
+      app.listen(process.env.PORT || 3000);
   })
   .catch((err) => {
     console.log(err);
