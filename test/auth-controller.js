091216f062606cb78b12ade5a4762ac99ca1373e
Dhimas Prajaya <dhimasprajaya@gmail.com>
2021-08-11 18:10:55 +0700

    testing

---
+++ test/auth-controller.js
@@ -0,0 +1,82 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+const AuthController = require('../controllers/auth');
+
+describe('Auth Controller', function () {
+  before(function (done) {
+    mongoose
+      .connect(
+        'mongodb+srv://dhimas:katakunci@cluster0.jo6yi.mongodb.net/test-messages?retryWrites=true&w=majority',
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      )
+      .then((result) => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          post: [],
+          _id: '6110d4d74a21b61533fd380e',
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  it('should throw an error with code 500 if accessing the database fails', function (done) {
+    sinon.stub(User, 'findOne');
+    User.findOne.throws();
+
+    const req = {
+      body: {
+        email: 'test@test.com',
+        password: 'tester',
+      },
+    };
+
+    AuthController.login(req, {}, () => {}).then((result) => {
+      expect(result).to.be.an('error');
+      expect(result).to.have.property('statusCode', 500);
+      done();
+    });
+
+    User.findOne.restore();
+  });
+
+  it('should send a response with a valid user status for an existing user', function (done) {
+    const req = { userId: '6110d4d74a21b61533fd380e' };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status;
+      },
+    };
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.status.code).to.be.equal(200);
+      expect(res.userStatus).to.be.equal('I am new!');
+      done();
+    });
+  });
+
+  after(function (done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+});
